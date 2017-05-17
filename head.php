12b461c385f036d798e40368f1c34be58d86ee32
graydon-armstrong <seeds77222@hotmail.com>
2017-05-17 15:58:20 -0400

    Added Basic HTML5 Head

---
+++ head.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html class="no-js" lang="en">
+    
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="x-ua-compatible" content="ie=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="resources/css/styles.css?<?php echo rand(1,10000);?>">
+    
+    <title><?php echo $page_title; ?></title>
+    <meta name="description" content="<?php echo $page_description; ?>">
+</head>
+
+<body>
