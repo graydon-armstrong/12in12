513c96fdea8d6b0598ee1865bcff0febc742ade0
graydon-armstrong <seeds77222@hotmail.com>
2017-05-17 15:59:22 -0400

    Added Basic Nav with home page

---
+++ nav.php
@@ -0,0 +1,14 @@
+<div class="title-bar" data-responsive-toggle="nav-menu" data-hide-for="medium">
+    <button class="menu-icon" type="button" data-toggle="nav-menu"></button>
+    <div class="title-bar-title">Menu</div>
+</div>
+
+<nav class="top-bar" id="nav-menu">
+    <div class="top-bar-left">
+        <ul class="vertical medium-horizontal menu" data-responsive-menu="drilldown medium-dropdown">
+            <li><a href="index.php">Home</a></li>
+        </ul>
+    </div>
+    <div class="top-bar-right">
+    </div>
+</nav>
