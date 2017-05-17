c8a2e012bfda4a4c403fa62280d2f5367ecb5cb2
graydon-armstrong <seeds77222@hotmail.com>
2017-05-17 15:58:58 -0400

    Added Foundation connections
    -Added header connections to css files
    -Added Footer connections to js files

--- footer.php
+++ footer.php
@@ -2,6 +2,10 @@
     <p class='text-center'>&copy;2017 Graydon Armstrong</p>
 </footer>
 
+<script src="vendors/foundation/js/jquery.js"></script>
+<script src="vendors/foundation/js/what-input.js"></script>
+<script src="vendors/foundation/js/foundation.min.js"></script>
+<script src="vendors/foundation/js/app.js"></script>
 
 </body>
 
--- head.php
+++ head.php
@@ -5,6 +5,8 @@
     <meta charset="UTF-8">
     <meta http-equiv="x-ua-compatible" content="ie=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="vendors/foundation/css/foundation.min.css?<?php echo rand(1,10000);?>">
+    <link rel="stylesheet" href="vendors/foundation/css/app.css?<?php echo rand(1,10000);?>">
     <link rel="stylesheet" href="resources/css/styles.css?<?php echo rand(1,10000);?>">
     
     <title><?php echo $page_title; ?></title>
