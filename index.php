284885923216b8bb0eb4c278738419c2c8f1ae63
graydon-armstrong <seeds77222@hotmail.com>
2017-05-17 17:02:13 -0400

    Start of Basic Framework for 12in12 List

--- index.php
+++ index.php
@@ -20,4 +20,37 @@
     </div>
 </div>
 
+<div class="row">
+    <div class="large-12 columns">
+        <h3>Games in 12in12</h3>
+        
+        <div class="primary progress">
+            <div class="progress-meter" style="width: 16.66%">
+                <p class="progress-meter-text">2/12</p>
+            </div>
+        </div>
+        
+        <div class="game">
+            <h4 class="gamename">Mass Effect 2</h4>
+            <img src="resources/images/masseffect2.jpg">
+        </div>
+    </div>
+</div>
+
+<div class="row">
+    <div class="large-12 columns">
+        <h3>Completed Games</h3>
+        
+        <div class="game">
+            <h4 class="gamename">Abzu</h4>
+            <img src="resources/images/abzu.jpg">
+        </div>
+        
+        <div class="game">
+            <h4 class="gamename">Refunct</h4>
+            <img src="resources/images/refunct.jpg">
+        </div>
+    </div>
+</div>
+
 <?php include 'footer.php'; ?>
