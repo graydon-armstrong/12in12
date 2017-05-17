8772e61b84a064021223181b87c963347da249b5
graydon-armstrong <seeds77222@hotmail.com>
2017-05-17 15:59:35 -0400

    Basic HTML Index page

---
+++ index.php
@@ -0,0 +1,23 @@
+<?php
+    $page_title = '12in12';
+    $page_description = 'A site for completing a 12in12 Steam Backlog';
+    
+    include 'head.php';    
+    include 'header.php';
+    include 'nav.php';
+?>
+
+<div class="row">
+    <div class="large-12 columns">
+        <h1>Welcome to 12in12</h1>
+    </div>
+</div>
+
+<div class="row">
+    <div class="large-12 columns">
+        <h3>Proof of Concept</h3>
+        <p>This is a proof in concept of a backlog manager for the 12in12 system that has its own community on reddit.</p>
+    </div>
+</div>
+
+<?php include 'footer.php'; ?>
